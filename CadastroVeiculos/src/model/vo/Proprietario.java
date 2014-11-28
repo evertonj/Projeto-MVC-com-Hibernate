70632e0d2edede70ee2ea0fdf4936560a609acfe
aalano <aalano@10.151.50.28>
2014-11-28 14:29:19 -0200

    criado gets nas views

--- CadastroVeiculos/src/model/vo/Proprietario.java
+++ CadastroVeiculos/src/model/vo/Proprietario.java
@@ -10,5 +10,54 @@ package model.vo;
  * @author Everton Spindola
  */
 public class Proprietario {
+    private int id;
+    private String nome;
+    private String fone;
+    private String cpf;
+    private String email;
+
+    public Proprietario() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
     
 }
