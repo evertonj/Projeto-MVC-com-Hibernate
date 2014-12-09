0b691a9cb61bffc2f6db4c86da16eb429f1f565b
aalano <aalano@192.168.1.8>
2014-12-09 20:46:07 -0200

    Inserir e atualizar proprietário já estão funcionando, também já fiz o veiculoBO

--- CadastroVeiculos/src/controller/Controller.java
+++ CadastroVeiculos/src/controller/Controller.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionListener;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 import model.bo.ProprietarioBO;
 import model.vo.Endereco;
 import model.vo.Proprietario;
@@ -36,6 +37,8 @@ public class Controller implements ActionListener{
         this.cdProp = new DlgCadastroProprietarios(frmPrincipal, true);
         this.cdVeiculo = new DlgCadastroVeiculo(frmPrincipal, true);
         this.frmPrincipal.getCdProp().addActionListener(this);
+        this.frmPrincipal.getCdVeiculo().addActionListener(this);
+        this.frmPrincipal.getSair().addActionListener(this);
         this.cdProp.getBtSalvar().addActionListener(this);
         this.cdProp.getBtAtualizar().addActionListener(this);
         this.cdProp.getBtCancelar().addActionListener(this);
@@ -50,18 +53,50 @@ public class Controller implements ActionListener{
        if(e.getSource() == this.frmPrincipal.getCdProp()) {
            this.cdProp.setVisible(true);
        }
+       if (e.getSource() == this.frmPrincipal.getSair()){
+           System.exit(0);
+       }
+       
        if(e.getSource() == this.cdProp.getBtSalvar()) {
            System.out.println("Passou!!!");
            carregarDadosAddProp();
            ProprietarioBO propBO = new ProprietarioBO();
            try {
                propBO.adicionar(this.prop);
+               JOptionPane.showMessageDialog(cdProp, "Proprietário cadastrado com sucesso!");
                this.cdProp.dispose();
            } catch (SQLException ex) {
                Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
            }
        }
-    }
+           if (e.getSource() == this.cdProp.getBtPesquisar()){
+               Long idProp = Long.valueOf(cdProp.getTfId().getText());
+               try 
+               {
+                   new ProprietarioBO().recuperar(idProp);
+                   setarDadosProp(idProp);
+               } catch (NullPointerException | SQLException ex) {
+                   JOptionPane.showMessageDialog(cdProp, "Proprietário não encontrado tente outro código!");
+                   Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+               }
+           }
+           
+           if (e.getSource() == this.cdProp.getBtAtualizar()){
+               carregarDadosAlterarProp();
+               try {
+                   new ProprietarioBO().alterar(prop);
+                   JOptionPane.showMessageDialog(cdProp, "Dados alterados com sucesso!!!");
+               } catch (SQLException ex) {
+                   Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+               }
+           }
+           if (e.getSource() == this.cdProp.getBtFechar()){
+                cdProp.dispose();
+           }
+           
+           
+       }
+    
 
     private void carregarDadosAddProp() {
         this.prop = new Proprietario(cdProp.getTfNome().getText(),
@@ -75,5 +110,41 @@ public class Controller implements ActionListener{
                         cdProp.getTfBairro().getText(),
                         cdProp.getTfEstado().getText(),
                         cdProp.getTfCidade().getText()));
+    }
+
+    private void carregarDadosAlterarProp() {
+        this.prop = new Proprietario(
+                Long.valueOf(cdProp.getTfId().getText()),
+                cdProp.getTfNome().getText(),
+                cdProp.getTfTelefone().getText(),
+                cdProp.getTfCPF().getText(),
+                cdProp.getTfEmail().getText()            
+        );
+        prop.setEndereco(new Endereco(cdProp.getTfCEP().getText(),
+                        cdProp.getTfLogradouro().getText(),
+                        cdProp.getTfNumero().getText(),
+                        cdProp.getTfBairro().getText(),
+                        cdProp.getTfEstado().getText(),
+                        cdProp.getTfCidade().getText()));
+    }
+    
+    private void setarDadosProp(long idProp) {
+        try {
+            // 1. Trazer o Proprietario do DB
+            prop  = new ProprietarioBO().recuperar(idProp);
+            // 2. Carregar os dados do proprieatario nos campos do dialog 
+            cdProp.getTfId().setText(String.valueOf(prop.getId()));
+            cdProp.getTfNome().setText(prop.getNome());
+            cdProp.getTfTelefone().setText(prop.getFone());
+            cdProp.getTfCPF().setText(prop.getCpf());
+            cdProp.getTfBairro().setText(prop.getEndereco().getBairro());
+            cdProp.getTfCEP().setText(prop.getEndereco().getCep());
+            cdProp.getTfCidade().setText(prop.getEndereco().getCidade());
+            cdProp.getTfNumero().setText(prop.getEndereco().getNumero());
+            cdProp.getTfEstado().setText(prop.getEndereco().getEstado());
+            cdProp.getTfLogradouro().setText(prop.getEndereco().getLogradouro());
+        } catch (SQLException ex) {
+            Logger.getLogger(Controller.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
--- CadastroVeiculos/src/model/bo/VeiculoBO.java
+++ CadastroVeiculos/src/model/bo/VeiculoBO.java
@@ -5,10 +5,53 @@
  */
 package model.bo;
 
+import java.sql.SQLException;
+import java.util.List;
+import model.dao.IDAO;
+import model.dao.VeiculoDAOMySQL;
+import model.vo.Veiculo;
+
 /**
  *
  * @author Everton Spindola
  */
-public class VeiculoBO {
+public class VeiculoBO implements IDAO<Veiculo>{
+    IDAO<Veiculo> idao;
+    public VeiculoBO() {
+        idao = new VeiculoDAOMySQL();
+    }
+
+    @Override
+    public void adicionar(Veiculo t) throws SQLException {
+        if(verificarDados(t))
+           idao.adicionar(t);
+    }
+
+    @Override
+    public void alterar(Veiculo t) throws SQLException {
+        if (verificarDados(t))
+            idao.alterar(t);
+    }
+
+    @Override
+    public Veiculo recuperar(Long id) throws SQLException {
+        return idao.recuperar(id);
+    }
+
+    @Override
+    public List<Veiculo> listarTodos() throws SQLException {
+       return idao.listarTodos();
+    }
+
+    @Override
+    public void remover(Veiculo t) throws SQLException {
+        idao.remover(t);
+    }
+
+    public boolean verificarDados(Veiculo t) {
+        return !t.getModelo().isEmpty() && !t.getMarca().isEmpty();
+    }
+    
+    
     
 }
